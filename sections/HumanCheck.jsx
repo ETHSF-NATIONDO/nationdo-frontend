759e89077f11ecdf8fd7227f3c1ebf61daef01a6
alexxriv <alex.trrzrivera@gmail.com>
2022-11-05 11:29:22 -0700

    worldcoin auth

--- sections/HumanCheck.jsx
+++ sections/HumanCheck.jsx
@@ -1,14 +1,42 @@
-import { useAccount } from '@web3modal/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react'
+import { useDisclosure } from '@chakra-ui/react'
+import WorldCoinAuth from './WorldCoinAuth'
 
 
-export default function UseAccount() {
-    const { account, isReady } = useAccount()
 
+export default function HumanCheck() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
     return (
-        <section>
-            <Button colorScheme='blue'>Human Check </Button>
-        </section>
+        <>
+            <Button onClick={onOpen} colorScheme='teal' variant='outline'>Human Check</Button>
 
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <WorldCoinAuth />
+
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant='ghost'>Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
